3ece63c4df680c0a1767d0f15e2310e5da548a66
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2021-02-13 01:10:18 -0300

    include tratation of return

--- src/main/java/com/relacionamento/relacionamento/Controller/SolicitacaoReembolsoController.java
+++ src/main/java/com/relacionamento/relacionamento/Controller/SolicitacaoReembolsoController.java
@@ -3,7 +3,10 @@ package com.relacionamento.relacionamento.Controller;
 import com.relacionamento.relacionamento.Entity.*;
 import com.relacionamento.relacionamento.Repository.ReembolsoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("solicitacao-reembolso")
@@ -13,9 +16,12 @@ public class SolicitacaoReembolsoController {
     ReembolsoRepository reembolsoRepository;
 
     @PostMapping
-    public String getSolicitacaoReembolso(@RequestBody Reembolso reembolso) {
-        System.out.println(reembolso.getDespesas().size());
-        reembolsoRepository.save(reembolso);
-        return "foi";
+    public ResponseEntity<Object> getSolicitacaoReembolso(@RequestBody @Valid Reembolso reembolso) {
+        try {
+            reembolsoRepository.save(reembolso);
+            return ResponseEntity.ok().body(new ReembolsoRetornoSucesso(reembolso));
+        } catch(Exception e) {
+            return ResponseEntity.ok().body(new ReembolsoRetornoErro(reembolso));
+        }
     }
 }
